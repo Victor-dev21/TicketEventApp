4595481f3ede4478e04ea2ecae98dd792e8f7616
github username <github email address>
2024-02-14 04:06:12 -0500

    Added event model

---
+++ db/migrate/20240214090557_create_events.rb
@@ -0,0 +1,17 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.integer :location_id
+      t.integer :admin_id
+      t.date :date
+      t.integer :category_id
+      t.integer :ticket_quantity
+      t.integer :venue_id
+      t.integer :ticket_availability
+      t.string--no-test-framework :status
+
+      t.timestamps
+    end
+  end
+end
